36f9afd1dd3d541ad58c1652f9f8c22bcb996d3d
acsa-laa <arcanjodagmar@gmail.com>
2019-08-03 21:07:20 -0300

    first commit

---
+++ cifrar.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int cifra;
+	int constante = 0;
+	int contador = 0;
+	string str;
+	char aux;
+	char vazio = ' ';
+	char *point;
+	char letras[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	string arquivo = argv[1];
+	ifstream obj(arquivo);
+	if (obj.is_open() == 0)
+	{
+	  ofstream obj;
+	  obj.open(arquivo);
+	}
+	
+	ofstream cifrado;
+	cifrado.open("mensagem.sac");
+
+	while(!obj.eof()) {
+		getline(obj, str);
+		for (int l = 0; l < str.size(); l++)
+		{
+			for (int i = 0; i < 26; i++)
+			{
+				if (str[l] == letras[i])
+				{	
+					constante = 1;
+					if (contador == 2 || contador == 5)
+					{
+						contador += 1;
+					}
+					else if(contador > 7)
+					{
+						contador = 0;
+					}
+					aux = (argv[2][contador]);
+					point = &(aux);
+					cifra = i + atoi(point);
+					if (cifra > 26)
+					{
+						cifra = cifra - 26;
+					}
+
+					cifrado << letras[cifra];
+				}
+			}
+			if (constante != 1)
+			{
+				cifrado << str[l];
+				contador--;
+			}
+		constante = 0;
+		contador++;
+		}
+			
+		
+	}
+
+	
+	return 0;
+}
